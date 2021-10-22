bbabf74836ef317568ec85f8c8b8d81cd016d4da
AuliaSyhrn <auliasyahrani299@gmail.com>
2021-10-22 20:57:40 +0700

    First Commit

---
+++ app/src/main/java/android/example/gomovie/config/ServerConfig.java
@@ -0,0 +1,6 @@
+package android.example.gomovie.config;
+
+public class ServerConfig {
+    public static final String URL_BASE = "https://developers.themoviedb.org";
+            public static final String API_ENDPOINT = URL_BASE;
+}
